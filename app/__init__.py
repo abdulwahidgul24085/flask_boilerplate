51be08dc649e60bf615b0cf0d26df5a132705bfc
abdulwahidgul24085 <abdulwahidgul24085@gmail.com>
2020-03-08 15:21:58 +0400

    config file and app factory added

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+mail = Mail()
+db = SQLAlchemy()
+
+# Application factory, takes config as a parameter var
+def create_app(config_name):
+    app = Flask(__name__)
+    # config from config.py can be imported in the app
+    # with from_object
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    mail.init_app(app,)
+    db.init_app(app)
+
+    # attach routes and custom error pages here
+
+    return app
