29abfff7f01533f5a79dde35c91ee804b578a912
abdulwahidgul24085 <abdulwahidgul24085@gmail.com>
2020-03-08 15:50:58 +0400

    blueprint view updated

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,4 @@ from .. import db
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        return redirect(url_for('main.index'))
     return render_template('index.html',)
